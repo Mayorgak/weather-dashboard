4f6f78d0bbdb9390d1fef1ed39d654a00865892c
Kristen M <kristenmayorga@gmail.com>
2020-07-12 14:16:49 -0400

    redo index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,67 @@
-function initPage () {
-    var inputEL = document.getElementById("city-input");
-    var searchEL = document.getElementById("earch-button")
-    var clearEl = document.getElementById("clear-history");
-    var nameEl = document.getElementById("city-name");
-    var currentPicEl = document.getElementById ("current-pic");
-    var currentTempEl = document.getElementById("temprature");
-    var currentHumdityEl = document.getElementById("#umidity");
-    var currentWindEl = document.getElementById("wind-speed");
-    var currentUVEL = document.getElementById("UV-index");
-    var historyEL = document.getElementById("history");
 
-    var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-     console.log(searchHistory);
-
-
-    var apiKey = "e8f5f64870bb4d6157cd0e7e73574b75";
-
-    function getWeather(cityName) {
     
-        var queryURL =
-          "http://api.openweathermap.org/data/2.5/weather?q=" +
-          cityName +
-          "&appid=" +
-          APIKey;
-        axios.get(queryURL)
-        .then(function(response){
-            console.log(response);
+    // Variable declaration
+    
+    var city ="";
+    
+    // var inputEl = document.querySelector("city-input");
+    // var searchEl = document.querySelector("searchbtn");
+    // var clearEl = document.querySelector("clear-history");
+
+    // var nameEl = document.querySelector("city-name");
+    // var currentTempEl = document.querySelector("temprature");
+    // var currentHumdityEl = document.querySelector("humidity");
+    // var currentWindEl = document.querySelector("wind-speed");
+    // var currentUVEL = document.querySelector("UV-index");
+    // var historyEL = document.querySelector("#history");
+    // var responseContainer = document.querySelector("#searchbtn")
+    // var inputEL = document.querySelector("#searchinput")
 
 
-    var currentDate = new Date(response.data.dt * 1000);
-        console.log(currentDate);   
+
+
+    // var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    //  console.log(searchHistory);
 
 
 
-        });
-    }
-}
-    initPage();
+    var getWeather= function () {
+      fetch(api.openweathermap.org/data/2.5/weather?q=)
+    };
+
+    getWeather();
+
+    // // query selector for search button
+    // // event listner for search button
+
+    // // query selector input text value
+    // // make sure fetch is working 
+
+    
+
+    // var getWeather = function(cityName) {
+        
+    //     var apiKey = "886705b4c1182eb1c69f28eb8c520e20";
+    
+    //     var queryURL =
+    //       "http://api.openweathermap.org/data/2.5/weather?q=" +
+    //       cityName +
+    //       "&appid=" +
+    //       apiKey;
+
+    //     fetch(queryURL)
+    //       .then(function (response) {
+    //         return response.json();
+    //       })
+    //       .then(function (data) {
+    //         console.log(data);
+
+    //         var currentDate = new Date(response.data.dt * 1000);
+    //         console.log(currentDate);
+    //       });
+    // }
+
+
+
+
+  
